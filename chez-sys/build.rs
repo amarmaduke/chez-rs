51be1a43859cd380936690cc65d285e22cf66824
Andrew Marmaduke <marmaduke.andrew@gmail.com>
2022-01-31 05:16:20 -0600

    remove ChezScheme submodule

--- chez-sys/build.rs
+++ chez-sys/build.rs
@@ -4,29 +4,31 @@ use std::process::Command;
 use std::env;
 
 fn main() {
-    let cargo_manifest = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let chez_source_path = PathBuf::from(cargo_manifest).join("ChezScheme");
     let cargo_output = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let chez_build_path = cargo_output.join("chez");
-    Command::new("cp")
-        .current_dir(&chez_source_path)
-        .arg("-r")
-        .arg(".")
-        .arg(&chez_build_path)
+    let chez_source_path = cargo_output.join("ChezScheme");
+    Command::new("git")
+        .current_dir(&cargo_output)
+        .arg("clone")
+        .arg("--depth")
+        .arg("1")
+        .arg("--branch")
+        .arg("v9.5.6")
+        .arg("https://github.com/cisco/ChezScheme.git")
         .status()
         .expect("Failed to copy Chez Scheme source files");
     Command::new("./configure")
-        .current_dir(&chez_build_path)
+        .current_dir(&chez_source_path)
         .arg("--disable-curses")
         .arg("--disable-x11")
+        .arg("-m=a6le")
         .arg("--libkernel")
         .status()
         .expect("Configuration failed!");
     Command::new("make")
-        .current_dir(&chez_build_path)
+        .current_dir(&chez_source_path)
         .status()
         .expect("Building failed!");
-    let chez_output = chez_build_path.join("a6le");
+    let chez_output = chez_source_path.join("a6le");
     let chez_kernel = chez_output.join("boot/a6le");
     let chez_lz4_lib = chez_output.join("lz4/lib");
     let chez_z_lib = chez_output.join("zlib");
